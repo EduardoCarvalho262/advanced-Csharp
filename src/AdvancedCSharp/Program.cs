56d4bfe7dd1b45ddfde57f09d0ed0eb1015fffb0
Eduardo Carvalho <eduardoc313@gmail.com>
2023-05-29 19:14:05 -0300

    Testando comportamento de um metodo com corpotamento diferente e parâmetros iguais

--- src/AdvancedCSharp/Program.cs
+++ src/AdvancedCSharp/Program.cs
@@ -1,6 +1,9 @@
 ﻿namespace AdvancedCSharp
 {
     delegate int DelegateWithTwoIntParameterReturnInt(int x, int y); //Declaração de um delegate
+    
+    // Ele é geralmente usado para lidar com eventos e chamadas de métodos
+    // principamente em programação assincrona.
 
     class Program
     {
@@ -9,23 +12,30 @@
             return a + b;
         }
 
+        public static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***A simple delegate demo.***");
             Console.WriteLine("\n Calling Sum(..) method without using a delegate:");
-            Console.WriteLine("Sum of 10 and 20 is : {0}", Sum(10, 20));
+            //Console.WriteLine("Sum of 10 and 20 is : {0}", Sum(10, 20));
+            Console.WriteLine("Multi of 2 and 3 is : {0}", Multi(2, 3));
             //Creating a delegate instance
             //DelegateWithTwoIntParameterReturnInt delOb = new DelegateWithTwoIntParameterReturnInt(Sum);
             //Or,simply write as follows:
-            DelegateWithTwoIntParameterReturnInt delOb = Sum;
-            Console.WriteLine("\nCalling Sum(..) method using a delegate.");
-            int total = delOb(10, 20);
-            Console.WriteLine("Sum of 10 and 20 is: {0}", total);
+            DelegateWithTwoIntParameterReturnInt delOb = Multi;
+            Console.WriteLine("\nCalling Multi(..) method using a delegate.");
+            int total = delOb(2, 3);
+            //Console.WriteLine("Sum of 10 and 20 is: {0}", total);
+            Console.WriteLine("Multi of 2 and 3 is: {0}", total);
             /* Alternative way to calculate the aggregate of the numbers.*/
             //delOb(25,75) is shorthand for delOb.Invoke(25,75)
-            Console.WriteLine("\nUsing Invoke() method on delegate instance, calculating sum of 25 and 75.");
-            total = delOb.Invoke(25, 75);
-            Console.WriteLine("Sum of 25 and 75 is: {0}", total);
+            Console.WriteLine("\nUsing Invoke() method on delegate instance, calculating Multi of 2 and 3.");
+            total = delOb.Invoke(2, 3);
+            Console.WriteLine("Multi of 2 and 3 is: {0}", total);
             Console.ReadKey();
         }
     }
