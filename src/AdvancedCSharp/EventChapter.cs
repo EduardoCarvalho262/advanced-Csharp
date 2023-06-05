975c9abe3f6b75600c90010b86e140f75dcf78cc
Eduardo Carvalho <eduardoc313@gmail.com>
2023-06-05 20:10:46 -0300

    Demonstration 1 of events

---
+++ src/AdvancedCSharp/EventChapter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharp
+{
+    public class EventChapter
+    {
+    }
+
+    public class Sender
+    {
+        public event EventHandler? MyIntChanged;
+
+        private int myInt;
+        public int MyInt
+        {
+            get
+            {
+                return myInt;
+            }
+
+            set
+            {
+                myInt = value;
+                OnMyIntChanged();
+            }
+        }
+
+        private void OnMyIntChanged()
+        {
+            if(MyIntChanged != null)   
+                MyIntChanged(this, EventArgs.Empty);
+        }
+
+        public void GetNotifications(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Sender himself send a notification: i have changed myint value to {0}", myInt);
+        }
+    }
+
+    public class Receiver
+    {
+        public void GetNotificationFromSender(Object sender, EventArgs e)
+        {
+            Console.WriteLine("Receiver receives a notification: Sender recently has chagned the myInt values.");
+        }
+    }
+
+}
--- src/AdvancedCSharp/Program.cs
+++ src/AdvancedCSharp/Program.cs
@@ -19,24 +19,41 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("***A simple delegate demo.***");
-            Console.WriteLine("\n Calling Sum(..) method without using a delegate:");
-            //Console.WriteLine("Sum of 10 and 20 is : {0}", Sum(10, 20));
-            Console.WriteLine("Multi of 2 and 3 is : {0}", Multi(2, 3));
-            //Creating a delegate instance
-            //DelegateWithTwoIntParameterReturnInt delOb = new DelegateWithTwoIntParameterReturnInt(Sum);
-            //Or,simply write as follows:
-            DelegateWithTwoIntParameterReturnInt delOb = Multi;
-            Console.WriteLine("\nCalling Multi(..) method using a delegate.");
-            int total = delOb(2, 3);
-            //Console.WriteLine("Sum of 10 and 20 is: {0}", total);
-            Console.WriteLine("Multi of 2 and 3 is: {0}", total);
+            //Console.WriteLine("***A simple delegate demo.***");
+            //Console.WriteLine("\n Calling Sum(..) method without using a delegate:");
+            ////Console.WriteLine("Sum of 10 and 20 is : {0}", Sum(10, 20));
+            //Console.WriteLine("Multi of 2 and 3 is : {0}", Multi(2, 3));
+            ////Creating a delegate instance
+            ////DelegateWithTwoIntParameterReturnInt delOb = new DelegateWithTwoIntParameterReturnInt(Sum);
+            ////Or,simply write as follows:
+            //DelegateWithTwoIntParameterReturnInt delOb = Multi;
+            //Console.WriteLine("\nCalling Multi(..) method using a delegate.");
+            //int total = delOb(2, 3);
+            ////Console.WriteLine("Sum of 10 and 20 is: {0}", total);
+            //Console.WriteLine("Multi of 2 and 3 is: {0}", total);
             /* Alternative way to calculate the aggregate of the numbers.*/
             //delOb(25,75) is shorthand for delOb.Invoke(25,75)
-            Console.WriteLine("\nUsing Invoke() method on delegate instance, calculating Multi of 2 and 3.");
-            total = delOb.Invoke(2, 3);
-            Console.WriteLine("Multi of 2 and 3 is: {0}", total);
+            //Console.WriteLine("\nUsing Invoke() method on delegate instance, calculating Multi of 2 and 3.");
+            //total = delOb.Invoke(2, 3);
+            //Console.WriteLine("Multi of 2 and 3 is: {0}", total);
+            //Console.ReadKey();
+
+
+            Console.WriteLine("******* Exploring Events *************");
+            Sender sender = new Sender();
+            Receiver receiver = new Receiver();
+
+            sender.MyIntChanged += receiver.GetNotificationFromSender;
+            sender.MyInt = 1;
+            sender.MyInt = 2;
+            //Unregistering now
+            sender.MyIntChanged -= receiver.GetNotificationFromSender;
+            sender.MyInt = 3;
+
+            sender.MyIntChanged += sender.GetNotifications;
+            sender.MyInt = 4;
             Console.ReadKey();
+
         }
     }
 }
